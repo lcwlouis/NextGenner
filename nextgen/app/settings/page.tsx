5aaaffb16804ded3545f019e4949c701dbb69fd4
Louis Liu <li0002is@e.ntu.edu.sg>
2024-06-24 16:36:08 +0800

    Added some amount of UI
    Sorry for the very bad documentation. Still experimenting with Next.js and Shadcn.
    
    Added page:
    - Settings
    
    Updated pages:
    - Landing
        |_ Removed original starting Next.js app content
    - Chat
        |_ Added sidebar
        |_ Added Navbar to top
        |_ Added textbox and button

--- nextgen/app/settings/page.tsx
+++ nextgen/app/settings/page.tsx
@@ -2,7 +2,6 @@
 // import shit for Shadcn UI
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import Link from "next/link"
 // import shit for react
 import { useState } from 'react'
@@ -26,7 +25,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
             <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
                 <div className="mx-auto grid w-full max-w-6xl gap-2">
-                <h1 className="text-3xl font-semibold">Chats</h1>
+                <h1 className="text-3xl font-semibold">Settings</h1>
                 <h1>This is the chatting page</h1>
                 </div>
                 <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
@@ -34,20 +33,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                         className="grid gap-4 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0"
                     >
                         <Link href="#" className="font-semibold text-primary">
-                        New Chat
+                        General
                         </Link>
-                        <Link href="/chat/1">Previous chat</Link>
+                        <Link href="#">Security</Link>
+                        <Link href="#">Integrations</Link>
+                        <Link href="#">Support</Link>
+                        <Link href="#">Organizations</Link>
+                        <Link href="#">Advanced</Link>
                     </nav>
-                    <div className="grid w-full gap-2">
-                        <ScrollArea>
-                            Test Text
-                        </ScrollArea>
-                        <Textarea placeholder="Type your message here." />
-                        <Button>Send message</Button>
-                    </div>  
                 </div>
-                
             </main>
+
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        
+            <div className="grid w-full gap-2">
+                <Textarea placeholder="Type your message here." />
+                <Button>Send message</Button>
+            </div>
+        </main>
         </div>
     );
 }
